c6c49cb2e5be4bfcd03e9c483bd99ae494c8c975
argentum844 <58563250+argentum844@users.noreply.github.com>
2020-04-03 11:14:13 +0300

    Add files via upload
    добавил main_file

---
+++ main_file.py
@@ -0,0 +1,30 @@
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import random
+from analysis import Analysis
+from my_token import TOKEN
+
+
+def main():
+    vk_session = vk_api.VkApi(
+        token=TOKEN)
+
+    longpoll = VkBotLongPoll(vk_session, '193289108')
+
+    for event in longpoll.listen():
+
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            print(event)
+            #print('Новое сообщение:')
+            #print('Для меня от:', event.obj.message['from_id'])
+            #print('Текст:', event.obj.message['text'])
+            vk = vk_session.get_api()
+            #print(event.obj.client_info)
+            message = Analysis(event.obj.message['text']).get_result()
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=message,
+                             random_id=random.randint(0, 2 ** 64))
+
+
+if __name__ == '__main__':
+    main()
