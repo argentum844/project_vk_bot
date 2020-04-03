67df55e8063d44a039668e7eb9f10dbdc7c88d0f
argentum844 <58563250+argentum844@users.noreply.github.com>
2020-04-03 11:48:37 +0300

    добавил анализатору базовый функционал

--- analysis.py
+++ analysis.py
@@ -1,6 +1,19 @@
 class Analysis:
     def __init__(self, text):
-        self.text = text
+        self.text = text.lower()
+        self.greeting = 'привет здравствуй здорово прив hi hello хай'.split()
+        self.commands = 'Карта места, Карта населенного пункта, Что нибудь ещё'.split(', ')
+        self.result = self.analys()
 
     def get_result(self):
-        return self.text
+        return self.result
+
+    def analys(self):
+        if any([x in self.text for x in self.greeting]):
+            return "привет"
+        if "команды" in self.text:
+            res = ''
+            for i in self.commands:
+                res += i + '\n'
+            return res
+        return "чтобы посмотреть возможные команды, напиши слово \"Команды\""
